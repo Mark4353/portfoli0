6b01974d38066a011879bd3cadc8496a3d041b07
“[Mark4353]” <130235333+Mark4353@users.noreply.github.com>
2025-09-07 20:23:11 +0300

    fix 1page

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import Projects from "./components/Projects";
 import CurrentProjects from "./components/CurrentProjects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import About from "./components/About";
+import Skills from "./components/Skills";
 // import Video from "./components/pages/Video"; // пока убираем роутинг
 
 function App() {
   return (
     <>
       <Hero />
+      <About />
+      <Skills />
       <Projects />
       <CurrentProjects />
       <Contact />
