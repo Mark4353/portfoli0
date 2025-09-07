52f5655cf811ae84e0d05f20eae751887bdf046e
“[Mark4353]” <130235333+Mark4353@users.noreply.github.com>
2025-09-07 20:18:32 +0300

    fix 2page

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
 import CurrentProjects from "./components/CurrentProjects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Video from "./components/pages/Video";
-import "./App.css";
+// import Video from "./components/pages/Video"; // пока убираем роутинг
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              
-              <Hero />
-              <Projects />
-              <CurrentProjects />
-              <Contact />
-              <Footer />
-            </>
-          }
-        />
-        <Route path="/video" element={<Video />} /> {/* новая страница */}
-      </Routes>
-    </Router>
+    <>
+      <Hero />
+      <Projects />
+      <CurrentProjects />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
